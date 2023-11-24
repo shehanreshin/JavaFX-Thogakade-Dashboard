55c106c17d04db8fbe8960a003a42f9f1c7ac66d
shehanreshin <asp.shehanreshin@gmail.com>
2023-11-24 20:00:34 +0530

    feat: make digital clock on the home page work

--- thogakade-dashboard/src/main/java/controller/MainDashboardController.java
+++ thogakade-dashboard/src/main/java/controller/MainDashboardController.java
@@ -1,18 +1,25 @@
 package controller;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
 public class MainDashboardController implements Initializable {
@@ -41,9 +48,22 @@ public class MainDashboardController implements Initializable {
     @FXML
     private JFXButton btnMinimize;
 
+    @FXML
+    private Label lblTime;
+    @FXML
+    private Label lblDayOfTheWeek;
+    @FXML
+    private Label lblAmOrPm;
+    @FXML
+    private Label lblDay;
+    @FXML
+    private Label lblMonth;
+    @FXML
+    private Label lblYear;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        calculateTime();
     }
 
     public void exit() {
@@ -92,5 +112,26 @@ public class MainDashboardController implements Initializable {
         Scene scene = new Scene(FXMLLoader.load(getClass().getResource(url)));
         scene.setFill(Color.TRANSPARENT);
         return scene;
+    }
+
+    private void calculateTime() {
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.ZERO,
+                        actionEvent -> {
+                            LocalDateTime localDateTime = LocalDateTime.now();
+                            lblTime.setText(localDateTime.format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+                            lblDay.setText(String.valueOf(localDateTime.getDayOfMonth()));
+                            lblMonth.setText(String.valueOf(localDateTime.getMonth()));
+                            lblYear.setText(String.valueOf(localDateTime.getYear()));
+                            lblDayOfTheWeek.setText(String.valueOf(localDateTime.getDayOfWeek()).substring(0,3)+",");
+                            String ampm = localDateTime.format(DateTimeFormatter.ofPattern("HH:mm:a"));
+                            lblAmOrPm.setText(
+                                    ampm.substring(ampm.length()-2, ampm.length())
+                            );
+                        }
+                ), new KeyFrame(Duration.seconds(1))
+        );
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
     }
 }
